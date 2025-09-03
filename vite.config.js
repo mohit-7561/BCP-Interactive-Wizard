c6b6b7278f84da64079101b1c141b68e254c49dc
mohit-7561 <mohitsinghania666r@gmail.com>
2025-09-03 18:20:27 +0530

    added deploy config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/BCP-Interactive-Wizard",
   css: {
     postcss: {
       plugins: [
